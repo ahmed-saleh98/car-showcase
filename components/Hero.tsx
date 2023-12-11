38ae1321600d7715cf9c21493543f6c5fd0ae5e7
ahmed-saleh98 <ahmed.mohamed.saleh1998@gmail.com>
2023-12-11 23:21:22 +0200

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x"></div>
+    </div>
+  );
+};
+
+export default Hero;
